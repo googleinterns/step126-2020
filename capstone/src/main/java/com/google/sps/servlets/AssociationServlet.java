5f85ff1cec4a7e3abdf21e3472575fa2d0daaaa4
Prashanti Anderson <psunander@gmail.com>
2020-06-24 01:24:19 +0000

    Added servlet to fetch hard coded association data from and display the data using js

--- capstone/src/main/java/com/google/sps/servlets/AssociationServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/AssociationServlet.java
@@ -14,19 +14,30 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
+import com.google.sps.data.AssociationData;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
+/** Servlet to fetch association data with most positive and negative associations */
+@WebServlet("/associations")
+public class AssociationServlet extends HttpServlet {
+
+  private static final String OUTPUT_TYPE = "applications/json;";
+  private static final ArrayList<String> positive = new ArrayList(Arrays.asList("test 1", "test 2", "test 3"));
+  private static final ArrayList<String> negative = new ArrayList(Arrays.asList("yeet 1", "yeet 2", "yeet 3"));
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.setContentType(OUTPUT_TYPE);
+
+    AssociationData output = new AssociationData(positive, negative);
+    Gson gson = new Gson();
+    response.getWriter().println(gson.toJson(output));
   }
 }
--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -64,3 +64,23 @@ function createMap() {
   precinctSix.setMap(map);
   precinctIsland.setMap(map);
 }
+async function associationUpdateDisplay() {
+  let response = await fetch('/associations');
+  let associations = await response.json();
+  
+  let positive = document.getElementById('pos-associations');
+  positive.innerHTML = '';
+  associations.positive.forEach(elem => addListElement(positive, elem));
+
+  let negative = document.getElementById('neg-associations');
+  negative.innerHTML = '';
+  associations.negative.forEach(elem => addListElement(negative, elem));
+}
+
+function addListElement(list, contents) {
+  const elem = document.createElement('li');
+  elem.textContent = contents;
+  list.appendChild(elem);
+}
+
+window.addEventListener('load', associationUpdateDisplay)
