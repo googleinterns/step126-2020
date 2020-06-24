702f61178ba78b89b2ac5e528adfae84a47c45c2
Zoe <mitchellzh101@gmail.com>
2020-06-24 19:38:16 +0000

    added map to homepage

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -1,1 +1,11 @@
-//some js code here
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map-container'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+
+  const trexMarker = new google.maps.Marker({
+    position: {lat: 37.421903, lng: -122.084674},
+    map: map,
+    title: 'Stan the T-Rex'
+  });
+}
