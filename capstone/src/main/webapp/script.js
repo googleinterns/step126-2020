f5961df7e23817f563abb54e8f8e8337c5c2a580
Prashanti Anderson <psunander@gmail.com>
2020-06-30 19:03:35 +0000

    Fixed js formatting

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -3,52 +3,52 @@ function createMap() {
       document.getElementById('map-container'),
       {center: {lat: 37.7749, lng: -122.4194}, zoom: 13});
 
-	    //**adding zipcode overlay */
-        map.data.loadGeoJson('zipcode-data.json');
-        map.data.setStyle({visible: false});
+  //* *adding zipcode overlay */
+  map.data.loadGeoJson('zipcode-data.json');
+  map.data.setStyle({visible: false});
 
-    //**cordinates for shapes */
-    var cityLimits = [
-        {lat: 37.708305, lng: -122.502691},
-        {lat: 37.708229, lng: -122.393322}
-    ]
+  //* *cordinates for shapes */
+  const cityLimitsCoordinates = [
+    {lat: 37.708305, lng: -122.502691},
+    {lat: 37.708229, lng: -122.393322},
+  ];
 
-    var precinctEleven = [
-        {lat: 37.724393, lng: -122.425343},
-        {lat: 37.725659, lng: -122.421427},
-        {lat: 37.724466, lng: -122.420893},
-        {lat: 37.724703, lng: -122.419927},
-        {lat: 37.723493, lng: -122.419458},
-        {lat: 37.723781, lng: -122.418436},
-        {lat: 37.722576, lng: -122.417942},
-        {lat: 37.722821, lng: -122.416941},
-        {lat: 37.721932, lng: -122.416552},
-        {lat: 37.722230, lng: -122.415586},
-        {lat: 37.723092, lng: -122.415967},
-        {lat :37.722815, lng: -122.424100},
-        {lat: 37.722860, lng: -122.426518}
-    ]
+  const precinctElevenCoordinates = [
+    {lat: 37.724393, lng: -122.425343},
+    {lat: 37.725659, lng: -122.421427},
+    {lat: 37.724466, lng: -122.420893},
+    {lat: 37.724703, lng: -122.419927},
+    {lat: 37.723493, lng: -122.419458},
+    {lat: 37.723781, lng: -122.418436},
+    {lat: 37.722576, lng: -122.417942},
+    {lat: 37.722821, lng: -122.416941},
+    {lat: 37.721932, lng: -122.416552},
+    {lat: 37.722230, lng: -122.415586},
+    {lat: 37.723092, lng: -122.415967},
+    {lat: 37.722815, lng: -122.424100},
+    {lat: 37.722860, lng: -122.426518},
+  ];
 
-    var precinctTen = [
-        {lat: 37.708263, lng: -122.428442},
-        {lat: 37.708702, lng: -122.428171},
-        {lat: 37.709288, lng: -122.428418},
-        {lat: 37.709606, lng: -122.429271},
-        {lat: 37.710596, lng: -122.431476},
-        {lat: 37.712300, lng: -122.429680},
-        {lat: 37.711408, lng: -122.427353},
-        {lat: 37.714243, lng: -122.425809},
-        {lat: 37.714991, lng: -122.426999},
-        {lat: 37.718564, lng: -122.426440},
-        {lat: 37.717077, lng: -122.424649},
-        {lat: 37.718979, lng: -122.420104},
-        {lat: 37.718040, lng: -122.414321},
-        {lat: 37.721717, lng: -122.400485},
-        {lat: 37.738258, lng: -122.408128},
-        {lat: 37.769080, lng: -122.408275}
-    ]
+  const precinctTenCoordinates = [
+    {lat: 37.708263, lng: -122.428442},
+    {lat: 37.708702, lng: -122.428171},
+    {lat: 37.709288, lng: -122.428418},
+    {lat: 37.709606, lng: -122.429271},
+    {lat: 37.710596, lng: -122.431476},
+    {lat: 37.712300, lng: -122.429680},
+    {lat: 37.711408, lng: -122.427353},
+    {lat: 37.714243, lng: -122.425809},
+    {lat: 37.714991, lng: -122.426999},
+    {lat: 37.718564, lng: -122.426440},
+    {lat: 37.717077, lng: -122.424649},
+    {lat: 37.718979, lng: -122.420104},
+    {lat: 37.718040, lng: -122.414321},
+    {lat: 37.721717, lng: -122.400485},
+    {lat: 37.738258, lng: -122.408128},
+    {lat: 37.769080, lng: -122.408275},
+  ];
 
-  var precinct6 = [
+  const precinctIslandCoordinates = [
     {lat: 37.826890, lng: -122.379144},
     {lat: 37.830382, lng: -122.377603},
     {lat: 37.832199, lng: -122.373619},
@@ -67,10 +67,10 @@ function createMap() {
     {lat: 37.807405, lng: -122.367401},
     {lat: 37.810965, lng: -122.372851},
     {lat: 37.814598, lng: -122.371302},
-    {lat: 37.826897, lng: -122.379137}
+    {lat: 37.826897, lng: -122.379137},
   ];
 
-  var precinctSix = [
+  const precinctSixCoordinates = [
     {lat: 37.793779, lng: -122.392673},
     {lat: 37.790398, lng: -122.388557},
     {lat: 37.790492, lng: -122.384992},
@@ -86,10 +86,10 @@ function createMap() {
     {lat: 37.787355, lng: -122.408318},
     {lat: 37.784468, lng: -122.407637},
     {lat: 37.784061, lng: -122.408281},
-    {lat: 37.782876, lng: -122.406493}
+    {lat: 37.782876, lng: -122.406493},
   ];
 
-  var precinctThree = [
+  const precinctThreeCoordinates = [
     {lat: 37.793723, lng: -122.392156},
     {lat: 37.804667, lng: -122.402254},
     {lat: 37.808092, lng: -122.408730},
@@ -101,67 +101,69 @@ function createMap() {
     {lat: 37.787377, lng: -122.408272},
     {lat: 37.784577, lng: -122.407559},
     {lat: 37.783999, lng: -122.408385},
-	{lat: 37.782885, lng: -122.406537}
+    {lat: 37.782885, lng: -122.406537},
   ];
 
-  /**drawing precincts */
- var cityLimits = new google.maps.Polygon({
-    paths: cityLimits,
+  /** drawing precincts */
+  const cityLimits = new google.maps.Polygon({
+    paths: cityLimitsCoordinates,
     strokeColor: 'black',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: 'black',
-    fillOpacity: 0.35
+    fillOpacity: 0.35,
   });
-  var precinctTen = new google.maps.Polygon({
-    paths: precinctTen,
+  const precinctTen = new google.maps.Polygon({
+    paths: precinctTenCoordinates,
     strokeColor: '#FFCF00',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: '#FFFC00',
-    fillOpacity: 0.35
+    fillOpacity: 0.35,
   });
-  var precinctIsland = new google.maps.Polygon({
-    paths: precinct6,
+  const precinctIsland = new google.maps.Polygon({
+    paths: precinctIslandCoordinates,
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: '#FF0000',
-    fillOpacity: 0.35
+    fillOpacity: 0.35,
   });
-  var precinctSix = new google.maps.Polygon({
-    paths: precinctSix,
+  const precinctSix = new google.maps.Polygon({
+    paths: precinctSixCoordinates,
     strokeColor: '#FF0000',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: '#FF0000',
-    fillOpacity: 0.35
+    fillOpacity: 0.35,
   });
-  var precinctThree = new google.maps.Polygon({
-    paths: precinctThree,
+  const precinctThree = new google.maps.Polygon({
+    paths: precinctThreeCoordinates,
     strokeColor: '#FFC000',
     strokeOpacity: 0.8,
     strokeWeight: 2,
     fillColor: '#FFC000',
-    fillOpacity: 0.35
+    fillOpacity: 0.35,
   });
+
   cityLimits.setMap(map);
   precinctThree.setMap(map);
   precinctSix.setMap(map);
   precinctIsland.setMap(map);
   precinctTen.setMap(map);
 }
+
 async function associationUpdateDisplay() {
   const response = await fetch('/associations');
   const associations = await response.json();
-  
+
   const positive = document.getElementById('pos-associations');
   positive.innerHTML = '';
-  associations.positive.forEach(elem => addListElement(positive, elem));
+  associations.positive.forEach((elem) => addListElement(positive, elem));
 
   const negative = document.getElementById('neg-associations');
   negative.innerHTML = '';
-  associations.negative.forEach(elem => addListElement(negative, elem));
+  associations.negative.forEach((elem) => addListElement(negative, elem));
 }
 
 function addListElement(list, contents) {
@@ -170,38 +172,40 @@ function addListElement(list, contents) {
   list.appendChild(elem);
 }
 
-window.addEventListener('load', associationUpdateDisplay)
+window.addEventListener('load', associationUpdateDisplay);
 
-function loadCharts(){
-    let stats = new google.visualization.DataTable();
-    
-    stats.addColumn('string', 'Sentiment');
-    stats.addColumn('number', 'Percentage');
-    stats.addRows([
-        ['Positive', 0.7],
-        ['Neutral', 0.1],
-        ['Negative', 0.2],
-    ]);
+function loadCharts() {
+  const stats = new google.visualization.DataTable();
 
-    // Instantiate and draw the chart.
-    let chart = new google.visualization.PieChart(document.getElementById('sentiment-pie-chart'));
-    chart.draw(stats, null);
+  stats.addColumn('string', 'Sentiment');
+  stats.addColumn('number', 'Percentage');
+  stats.addRows([
+    ['Positive', 0.7],
+    ['Neutral', 0.1],
+    ['Negative', 0.2],
+  ]);
 
-    loadReponseChart();
+  // Instantiate and draw the chart.
+  const chart = new google.visualization.PieChart(
+      document.getElementById('sentiment-pie-chart'));
+  chart.draw(stats, null);
+
+  loadReponseChart();
 }
 
-function loadReponseChart(){
-    let stats = new google.visualization.DataTable();
-    
-    stats.addColumn('string', 'Period');
-    stats.addColumn('number', 'Number of Responses');
+function loadReponseChart() {
+  const stats = new google.visualization.DataTable();
 
-    stats.addRows([
-        ['Daily', 4],
-        ['Weekly', 15]
-    ]);
+  stats.addColumn('string', 'Period');
+  stats.addColumn('number', 'Number of Responses');
 
-    // Instantiate and draw the chart.
-    let chart = new google.visualization.BarChart(document.getElementById('response-bar-chart'));
-    chart.draw(stats, null);
+  stats.addRows([
+    ['Daily', 4],
+    ['Weekly', 15],
+  ]);
+
+  // Instantiate and draw the chart.
+  const chart = new google.visualization.BarChart(
+      document.getElementById('response-bar-chart'));
+  chart.draw(stats, null);
 }
