e3c2e366dda889397383454c478fe1b1920865f2
Prashanti Anderson <psunander@gmail.com>
2020-07-01 20:29:17 +0000

    Fixed java formatting

--- capstone/src/main/java/com/google/sps/EntitySentiment.java
+++ capstone/src/main/java/com/google/sps/EntitySentiment.java
@@ -11,6 +11,7 @@ public class EntitySentiment {
 
   /**
    * Creates a new EntitySentiment object
+   *
    * @param content the word/phrase the object refers to
    * @param signficance the importance of the entity in the text
    * @param sentiment the sentiment associated with the entity
@@ -20,17 +21,19 @@ public class EntitySentiment {
     this.significance = significance;
     this.sentiment = sentiment;
   }
-  
+
   /**
    * Returns the content that the entity refers to
+   *
    * @return the word/phrase this entity refers to
-   **/
+   */
   public String getContent() {
     return content;
   }
 
   /**
    * Returns the importance of this entity in the text
+   *
    * @return importance of the entity in the text
    */
   public float getSignificance() {
@@ -39,6 +42,7 @@ public class EntitySentiment {
 
   /**
    * Returns a sentiment score from -1 (negative) to 1 (positive)
+   *
    * @return sentiment score for the entity represented
    */
   public float getSentiment() {
@@ -46,13 +50,13 @@ public class EntitySentiment {
   }
 
   /** Orders alphabetically by content */
-  public static final Comparator<EntitySentiment> ORDER_CONTENT = 
-    new Comparator<EntitySentiment>() {
-      @Override
-      public int compare(EntitySentiment a, EntitySentiment b) {
-        return a.getContent().compareTo(b.getContent());
-      }
-    };
+  public static final Comparator<EntitySentiment> ORDER_CONTENT =
+      new Comparator<EntitySentiment>() {
+        @Override
+        public int compare(EntitySentiment a, EntitySentiment b) {
+          return a.getContent().compareTo(b.getContent());
+        }
+      };
 
   @Override
   public String toString() {
--- capstone/src/test/java/com/google/sps/servlets/AssociationServletTest.java
+++ capstone/src/test/java/com/google/sps/servlets/AssociationServletTest.java
@@ -1,26 +1,25 @@
 package com.google.sps.servlet;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+
 import com.google.gson.Gson;
-import com.google.sps.servlets.AssociationServlet;
 import com.google.sps.data.AssociationData;
+import com.google.sps.servlets.AssociationServlet;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Arrays;
 import java.util.ArrayList;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-public class AssociationServletTest extends Mockito{
+public class AssociationServletTest extends Mockito {
 
   @Test
   public void testServlet() throws Exception {
-    HttpServletRequest request = mock(HttpServletRequest.class);       
-    HttpServletResponse response = mock(HttpServletResponse.class);    
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
 
     StringWriter stringWriter = new StringWriter();
     PrintWriter writer = new PrintWriter(stringWriter);
