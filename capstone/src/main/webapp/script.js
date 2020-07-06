d7f3c3635f906dd5d95e58d091236a42a261e4b2
Zoe <mitchellzh101@gmail.com>
2020-07-06 20:30:25 +0000

    adding global google

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/* global google */
+
 function createMap() {
   const map = new google.maps.Map(document.getElementById('map-container'),
       {
