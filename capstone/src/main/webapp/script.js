61925a15c92ebe24dd5f23789dec2d61a777b530
Zoe <mitchellzh101@gmail.com>
2020-07-06 19:11:04 +0000

    adding liciencing

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 function createMap() {
   const map = new google.maps.Map(document.getElementById('map-container'),
       {
@@ -23,17 +37,17 @@ function createMap() {
 
   //* *button for centering map*/
   const centerControlDiv = document.createElement('div');
-  const centerControl = new CenterControl(centerControlDiv, map);
+  var centerControl = new CenterControl(centerControlDiv, map);
   map.controls[google.maps.ControlPosition.LEFT_CENTER].push(centerControlDiv);
 
   //* *button for zipcode data layer */
   const zipControlDiv = document.createElement('div');
-  const zipControl = new ZipControl(zipControlDiv, map);
+  var zipControl = new ZipControl(zipControlDiv, map);
   map.controls[google.maps.ControlPosition.LEFT_CENTER].push(zipControlDiv);
 
   //* *button for zipcode data layer */
   const precinctControlDiv = document.createElement('div');
-  const precinctControl = new PrecinctControl(precinctControlDiv, map);
+  var precinctControl = new PrecinctControl(precinctControlDiv, map);
   map.controls[google.maps.ControlPosition.LEFT_CENTER]
       .push(precinctControlDiv);
 }
