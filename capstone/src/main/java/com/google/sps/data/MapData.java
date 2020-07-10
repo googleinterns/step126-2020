3029497640798fcdd07ce37bb656f26342b14c9f
Zoe <mitchellzh101@gmail.com>
2020-07-10 19:09:37 +0000

    sentimentData

--- capstone/src/main/java/com/google/sps/data/MapData.java
+++ capstone/src/main/java/com/google/sps/data/MapData.java
@@ -1,37 +1,42 @@
-public class Map {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapData {
   static final Map<String,ArrayList> zipPrecinctMap;
   static {
         zipPrecinctMap = new HashMap<>();
         zipPrecinctMap.put("94103", new ArrayList<>(
-            Arrays.asList("Southern","Mission"));
+            Arrays.asList("Southern","Mission")));
         zipPrecinctMap.put("94107", new ArrayList<>(
-            Arrays.asList("Bayview","Southern"));
+            Arrays.asList("Bayview","Southern")));
         zipPrecinctMap.put("94109", new ArrayList<>(
-            Arrays.asList("Tenderloin","Central","Northern"));
+            Arrays.asList("Tenderloin","Central","Northern")));
         zipPrecinctMap.put("94110", new ArrayList<>(
-            Arrays.asList("Ingleside","Mission"));
+            Arrays.asList("Ingleside","Mission")));
         zipPrecinctMap.put("94112", new ArrayList<>(
-            Arrays.asList("Ingleside","Taraval"));
+            Arrays.asList("Ingleside","Taraval")));
         zipPrecinctMap.put("94114", new ArrayList<>(
-            Arrays.asList("Park","Mission"));
+            Arrays.asList("Park","Mission")));
         zipPrecinctMap.put("94116", new ArrayList<>(
-            Arrays.asList("Taraval"));
+            Arrays.asList("Taraval")));
         zipPrecinctMap.put("94117", new ArrayList<>(
-            Arrays.asList("Park","Northern"));
+            Arrays.asList("Park","Northern")));
         zipPrecinctMap.put("94118", new ArrayList<>(
-            Arrays.asList("Richmond","Park"));
+            Arrays.asList("Richmond","Park")));
         zipPrecinctMap.put("94121", new ArrayList<>(
-            Arrays.asList("Richmond"));
+            Arrays.asList("Richmond")));
         zipPrecinctMap.put("94122", new ArrayList<>(
-            Arrays.asList("Richmond","Taraval","Park"));
+            Arrays.asList("Richmond","Taraval","Park")));
         zipPrecinctMap.put("94123", new ArrayList<>(
-            Arrays.asList("Northern"));
+            Arrays.asList("Northern")));
         zipPrecinctMap.put("94124", new ArrayList<>(
-            Arrays.asList("Bayview"));
+            Arrays.asList("Bayview")));
         zipPrecinctMap.put("94129", new ArrayList<>(
-            Arrays.asList("Richmond"));
+            Arrays.asList("Richmond")));
         zipPrecinctMap.put("94131", new ArrayList<>(
-            Arrays.asList("Ingleside","Park"));
+            Arrays.asList("Ingleside","Park")));
     }
     public ArrayList getPrecincts(String zipcode){
         return zipPrecinctMap.get(zipcode);
--- capstone/src/main/java/com/google/sps/data/ReadData.java
+++ capstone/src/main/java/com/google/sps/data/ReadData.java
@@ -7,9 +7,6 @@ import com.google.appengine.api.datastore.EntityNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ReadData {
   final String DELIMITER = ",";
