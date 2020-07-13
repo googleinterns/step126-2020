8a755399342a7cac9b47fcba6980bf41640d30fc
Zoe <mitchellzh101@gmail.com>
2020-07-13 13:43:00 +0000

    sentimentData

---
+++ capstone/src/main/java/com/google/sps/data/SentimentData.java
@@ -0,0 +1,15 @@
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+
+public class SentimentData{
+    public double getSentiment(String message){
+    Document doc =
+        Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+    return Double.parseDouble(new Float(score).toString());
+    }
+}
