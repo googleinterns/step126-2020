23ddc36e4df5b3999fa7994dd8987b678405bb4f
Nitin <nitinramadoss@gmail.com>
2020-07-14 00:15:42 +0000

    Reads each survey, adds zip code, fixes parsing error with commas being in raw response

--- capstone/src/main/java/com/google/sps/data/ReadData.java
+++ capstone/src/main/java/com/google/sps/data/ReadData.java
@@ -4,37 +4,70 @@ import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.sps.data.MapData;
 import com.google.sps.data.SentimentData;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.NumberFormatException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class ReadData {
-  final String DELIMITER = ",";
+  private DatastoreService datastoreService;
+  private SentimentData sentimentService;
+  private ArrayList<Entity> newEntities;
+  private final String DELIMITER = ",";
+
+  public ReadData() throws IOException {
+    datastoreService = DatastoreServiceFactory.getDatastoreService();
+    sentimentService = new SentimentData();
+    newEntities = new ArrayList<Entity>();
+  }
 
   /**
-   * Reads the survey csv and adds each element to datastore
-   *
-   * @return {Void}
-   */
-  public void readCSV() {
-    BufferedReader reader = null;
-    FileReader file = null;
-    DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
-    SentimentData sentimentService = new SentimentData();
-    ArrayList<Entity> newEntities = new ArrayList<Entity>();
+  * Calls read file while iterating through all the zip codes
+  * @return {Void}
+  */
+  public void readAll() {
+    Set<String> zipCodes = MapData.zipPrecinctMap.keySet();
+    
+    for (String zip : zipCodes) {
+        String filePath = "assets/" + zip + ".csv";
 
+        try {
+            readFile(new FileReader(filePath), zip);
+        } catch (FileNotFoundException e) {
+            System.out.println(filePath + " is not in the assets folder");
+        }
+    }
+    
+  }
+
+  /**
+  * Reads the survey csv and adds each element to datastore
+  * @param file This is the file that is being parsed
+  * @param zip This is the zip code the file is associated with
+  * @return {Void}
+  */
+  public void readFile(FileReader file, String zip) {
+    final int EXPECTED_LENGTH = 15;
+    BufferedReader reader = null;
 
     try {
-      file = new FileReader("assets/surveyresponse.csv");
       reader = new BufferedReader(file);
       String header = reader.readLine();
       String line = "";
-
+    
       while ((line = reader.readLine()) != null) {
         String[] values = line.split(DELIMITER);
         String id = values[0];
@@ -48,15 +81,24 @@ public class ReadData {
         }
 
         String completion = values[2];
-        String gender = values[5];
-        String ageRange = values[6];
-        String answerOne = values[9];
-        String answerTwo = values[10];
-        String answerThree = values[12];
-        double score = sentimentService.getSentiment(answerThree);
-        long responseTimeOne = Long.parseLong(values[13]);
-        long responseTimeTwo = Long.parseLong(values[14]);
-        long responseTimeThree = Long.parseLong(values[15]);
+        String gender = values[4];
+        String ageRange = values[5];
+        String answerOne = values[8];
+        String answerTwo = values[9];
+        String answerThree = "";
+
+        final int START_INDEX = 11;
+
+        int indexOfLong = (values.length - EXPECTED_LENGTH) + START_INDEX + 1;
+        
+        for (int i = START_INDEX; i < indexOfLong; i++) {
+            answerThree += values[i];
+        }
+        
+        float score = sentimentService.getSentiment(answerThree);
+        long responseTimeOne = Long.parseLong(values[indexOfLong++]);
+        long responseTimeTwo = Long.parseLong(values[indexOfLong++]);
+        long responseTimeThree = Long.parseLong(values[indexOfLong]);
 
         Entity entity = new Entity("Response", id);
         Entity inStore; // Entity that may or may not exist in Datastore
@@ -68,6 +110,7 @@ public class ReadData {
         }
 
         if (inStore == null) {
+          entity.setProperty("zipCode", zip);
           entity.setProperty("id", id);
           entity.setProperty("date", date);
           entity.setProperty("completion", completion);
@@ -88,7 +131,7 @@ public class ReadData {
        datastoreService.put(newEntities);
 
     } catch (IOException e) {
-      System.out.println("IO error");
+      System.out.println("Error parsing the csv");
     } finally {
       if (reader != null) {
         try {
@@ -98,6 +141,6 @@ public class ReadData {
         }
       }
       sentimentService.close();
-    }
+    }  
   }
 }
