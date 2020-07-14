90f7db700293de3d5cd67787421713db5122e552
Nitin <nitinramadoss@gmail.com>
2020-07-14 00:10:37 +0000

    Adds package statement

--- capstone/src/main/java/com/google/sps/data/SentimentData.java
+++ capstone/src/main/java/com/google/sps/data/SentimentData.java
@@ -1,17 +1,26 @@
+package com.google.sps.data;
+
 import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
 import java.io.IOException;
 
-public class SentimentData{
+public class SentimentData {
+    private static LanguageServiceClient languageService;
+    
+    public SentimentData() throws IOException {
+        languageService = LanguageServiceClient.create(); 
+    }
 
-    public double getSentiment(String message) throws IOException {
-    Document doc =
-        Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
-    LanguageServiceClient languageService = LanguageServiceClient.create();
-    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
-    float score = sentiment.getScore();
-    languageService.close();
-    return Double.parseDouble(new Float(score).toString());
+    public float getSentiment(String message) throws IOException {
+        Document doc =
+            Document.newBuilder().setContent(message).setType(Document.Type.PLAIN_TEXT).build();
+        Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+        float score = sentiment.getScore();
+        return score;
+    }
+
+    public void close(){
+        languageService.close();
     }
 }
