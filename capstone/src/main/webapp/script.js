9a37fc5b4d76fbb12bc1e755e6157328d5ede734
Prashanti Anderson <psunander@gmail.com>
2020-07-14 23:12:55 +0000

    Debugged code for associations for different precincts

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -146,6 +146,27 @@ function precinctControl(controlDiv, map) {
   });
 }
 
+let precinct = "SF";
+
+async function associationUpdateDisplay() {
+  const response = await fetch('/associations?scope=' + precinct);
+  const associations = await response.json();
+
+  const positive = document.getElementById('pos-associations');
+  positive.innerHTML = '';
+  associations.positive.forEach((elem) => addListElement(positive, elem));
+
+  const negative = document.getElementById('neg-associations');
+  negative.innerHTML = '';
+  associations.negative.forEach((elem) => addListElement(negative, elem));
+}
+
+function addListElement(list, contents) {
+  const elem = document.createElement('li');
+  elem.textContent = contents;
+  list.appendChild(elem);
+}
+
 window.addEventListener('load', createMap);
 window.addEventListener('load', postSurveyResponses);
 
