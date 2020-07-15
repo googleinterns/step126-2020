892434d4c5efc10b650662150fe8564d618822bd
Zoe <mitchellzh101@gmail.com>
2020-07-15 02:47:11 +0000

    suggested changes

--- capstone/src/main/java/com/google/sps/servlets/CheckUserStatus.java
+++ capstone/src/main/java/com/google/sps/servlets/CheckUserStatus.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/status")
-public class CheckStatus extends HttpServlet {
+public class CheckUserStatus extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
