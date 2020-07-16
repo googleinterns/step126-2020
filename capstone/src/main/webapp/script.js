29d498d66c845439449e26457f924591b6231e7e
Prashanti Anderson <psunander@gmail.com>
2020-07-16 00:12:21 +0000

    Added modal pop up for word cloud

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -354,3 +354,42 @@ async function showStats() {
   }
 }
 /* eslint-enable no-unused-vars */
+async function showStats() {
+  const logStatus = await fetch('/status');
+  const status = await logStatus.json();
+  if (!status) {
+    window.location.href = '/login';
+  } else {
+    window.location.replace('statistics.html');
+  }
+}
+
+function configModal() {
+  // Get the modal
+  const modal = document.getElementById('modal');
+  
+  // Get the button that opens the modal
+  const btn = document.getElementById('associations-container');
+
+  // Get the <span> element that closes the modal
+  const span = document.getElementById('modal-close');
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = 'block';
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = 'none';
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+}
+
+window.addEventListener('load', configModal);
