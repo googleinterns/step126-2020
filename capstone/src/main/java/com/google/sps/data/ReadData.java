da343084ee07faf3309c87845fa61feed029f271
Nitin <nitinramadoss@gmail.com>
2020-07-24 20:06:51 +0000

    Removes string formatters and solves linter errors

--- capstone/src/main/java/com/google/sps/data/ReadData.java
+++ capstone/src/main/java/com/google/sps/data/ReadData.java
@@ -130,13 +130,9 @@ public class ReadData {
           Precinct precinctData = FeatureData.getPrecinct(precinctName);
 
           if (precinctData != null) {
-            entity.setProperty(
-                "averageHouseholdIncome",
-                String.format("%.2f", precinctData.getAverageHouseholdIncome()));
-            entity.setProperty("crimeRate", String.format("%.2f", precinctData.getCrimeRate()));
-            entity.setProperty(
-                "policeStationRating",
-                String.format("%.2f", precinctData.getPoliceStationRating()));
+            entity.setProperty("averageHouseholdIncome", precinctData.getAverageHouseholdIncome());
+            entity.setProperty("crimeRate", precinctData.getCrimeRate());
+            entity.setProperty("policeStationRating", precinctData.getPoliceStationRating());
           }
 
           newEntities.add(entity);
