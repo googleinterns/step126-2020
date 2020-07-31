467138a8c2dac2ade51a3c49be0f9a02cd3d2c05
Nitin <nitinramadoss@gmail.com>
2020-07-31 19:06:06 +0000

    Adds documentation to servlet

--- capstone/src/main/java/com/google/sps/servlets/DataServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,7 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+/** 
+* Servlet that retreives entities from the ReadData class and adds them to 
+* data store if they are not already present
+*/
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   @Override
